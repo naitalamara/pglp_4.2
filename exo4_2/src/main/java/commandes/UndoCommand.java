8ca36b09124a731057fb7b17551c49078d023614
naitalamara <ouali.naitalamara@gmail.com>
2020-03-27 10:56:00 +0100

    cmd undo

--- exo4_2/src/main/java/commandes/UndoCommand.java
+++ exo4_2/src/main/java/commandes/UndoCommand.java
@@ -1,9 +1,20 @@
 package commandes;
 
+import java.util.Stack;
+
 public class UndoCommand implements Command  {
 
+	private Annulable_comd derniere_cmd ;
+	
+	public UndoCommand(Stack<Annulable_comd> historique  ) {
+		
+		this.derniere_cmd =historique.pop();
+		
+	}
+	
+	
 	public void executer() {
-		// TODO Auto-generated method stub
+		derniere_cmd.undo();
 		
 	}
 
