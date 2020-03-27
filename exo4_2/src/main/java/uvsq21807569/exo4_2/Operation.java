9217a25b34535f4d32db08d90375680565c5d755
naitalamara <ouali.naitalamara@gmail.com>
2020-03-27 11:39:57 +0100

    operation

---
+++ exo4_2/src/main/java/uvsq21807569/exo4_2/Operation.java
@@ -0,0 +1,59 @@
+package uvsq21807569.exo4_2;
+
+public enum Operation {
+	
+	PLUS("+"){
+
+		@Override
+		public double eval(double x, double y) {
+		return x+y;
+		}
+		},
+	MOINS("-"){
+		@Override
+
+		public double eval(double x, double y) {
+		return x - y;
+		}
+		},
+	MULT("*"){
+		@Override
+		public double eval(double x, double y) {
+			return x * y;
+
+			}
+},
+
+	DIV("/"){		
+
+		@Override
+
+			public double eval(double x, double y) {
+
+			return x / y;
+
+			}
+};
+		
+
+	private String sym;
+	private Operation(String sym) {
+		this.sym=sym;
+	}
+	
+	
+	public abstract double eval(double x , double y);
+	}
+
+		
+
+
+
+		
+
+
+
+			
+
+
+
